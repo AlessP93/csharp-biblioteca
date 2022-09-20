c88519f932d02df454eb6dd4be25c0c0253effbf
AlessP93 <alespiscino@libero.it>
2022-09-20 14:26:32 +0200

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// 1 biblioteca
+
+// 2 registrati al sistema
+
+
+// 3 utente registrato bool=true else false
+
+//cognome,
+//nome,
+//email,
+//password,
+//recapito telefonico,
+
+// 4 utente puo effettuare dei prestiti sui documenti (libri\dvd)
+
+//un codice identificativo di tipo stringa (ISBN per i libri, numero seriale per i DVD),
+//titolo,
+//anno,
+//settore(storia, matematica, economia, …),
+//stato(In Prestito, Disponibile),
+//uno scaffale in cui è posizionato,
+//un autore (Nome, Cognome).
