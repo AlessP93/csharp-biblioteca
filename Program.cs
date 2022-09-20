d24a44150d37dda9c89c7009825ecd89fc174630
AlessP93 <alespiscino@libero.it>
2022-09-20 16:01:48 +0200

    Implemenazione

--- Program.cs
+++ Program.cs
@@ -1,18 +1,11 @@
-﻿// 1 biblioteca
-
-// 2 registrati al sistema
-
-
-// 3 utente registrato bool=true else false
-
+﻿using System.Runtime.ConstrainedExecution;
+//Si vuole progettare un sistema per la gestione di una biblioteca. Gli utenti si possono registrare al sistema, fornendo:
 //cognome,
 //nome,
 //email,
 //password,
 //recapito telefonico,
-
-// 4 utente puo effettuare dei prestiti sui documenti (libri\dvd)
-
+//Gli utenti registrati possono effettuare dei prestiti sui documenti che sono di vario tipo (libri, DVD). I documenti sono caratterizzati da:
 //un codice identificativo di tipo stringa (ISBN per i libri, numero seriale per i DVD),
 //titolo,
 //anno,
@@ -20,3 +13,46 @@
 //stato(In Prestito, Disponibile),
 //uno scaffale in cui è posizionato,
 //un autore (Nome, Cognome).
+//Per i libri si ha in aggiunta il numero di pagine, mentre per i dvd la durata.
+//L’utente deve poter eseguire delle ricerche per codice o per titolo e, eventualmente, effettuare dei prestiti registrando il periodo (Dal/Al) del prestito e il documento.
+//Deve essere possibile effettuare la ricerca dei prestiti dato nome e cognome di un utente.
+
+Film film = new Film();
+film.titolo = "film";
+
+Books books = new Books();
+
+Biblioteca biblioteca = new Biblioteca();
+
+public class Biblioteca
+{
+    public class Documents
+    {
+        public string titolo;
+        public int anno;
+        public string settore;
+        public bool stato;
+        public int scaffale;
+        public string autore;
+    }
+
+    public class Film
+    {
+        public long durata;
+        public string titolo;
+        public int anno;
+        public string settore;
+        public string autore;
+    }
+    public class Books
+    {
+        public int pagine;
+        public string titolo;
+        public int anno;
+        public string settore;
+        public string autore;
+    }
+}
+
+
+
